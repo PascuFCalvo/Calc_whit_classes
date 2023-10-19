c1542f74296540dd2cf766b0066fc4eb3767ed2f
PascuFCalvo <pascual.fernandez.calvo@gmail.com>
2023-10-19 18:20:01 +0200

    calculator

---
+++ index.js
@@ -0,0 +1,14 @@
+const displayValorAnterior = document.getElementById('valor-anterior');
+const displayValorActual = document.getElementById('valor-actual');
+const botonesNumeros = document.querySelectorAll('.numero');
+const botonesOperaciones = document.querySelectorAll('.operacion');
+
+const display = new Display(displayValorAnterior, displayValorActual);
+
+botonesNumeros.forEach(boton => {
+    boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML));
+});
+
+botonesOperaciones.forEach(boton => {
+    boton.addEventListener('click', () => display.operar(boton.value))
+});
